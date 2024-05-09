f8bdf94c560b21ffc3778bac1f1857ffd004bd6f
briquet9 <88289097+briquet9@users.noreply.github.com>
2024-05-09 12:19:22 +0200

    Image slider initialized

---
+++ README.md
@@ -0,0 +1,19 @@
+# 3 examples of a simple image slider
+
+1. [Basic slide toggler using radio buttons and labels](example-1/)
+1. [Scroll slider using anchors and links](example-2/)
+1. [Scroll slider using radio buttons and Javascript](example-3/)
+
+
+#### Image credits
+
+##### Images
+
+* [Werner Sevenster: Road in between brown wooden fences](https://unsplash.com/photos/road-in-between-brown-wooden-fences-JuP0ZG0UNi0)
+* [Casey Horner: Waterfalls between green mountains at daytime](https://unsplash.com/photos/waterfalls-between-green-mountains-at-daytime-8ftuBebG3_M)
+* [Laura Smetsers: Body of water near field of trees](https://unsplash.com/photos/body-of-water-near-field-of-trees-H-TW2CoNtTk)
+* [Mathew Waters: Green leaf trees under white and blue cloudy sky](https://unsplash.com/photos/green-leaf-trees-under-white-and-blue-cloudy-sky-3ux6TnSApDs)
+
+##### SVG icons
+
+* [SVG Repo](https://www.svgrepo.com/)
---
+++ example-3/slider.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function arrowClicked(event, direction) {
+        var slides = event.target.parentElement.parentElement.parentElement.getElementsByClassName('slides')[0];
+        slides.scrollLeft += direction * slides.scrollWidth / slides.childElementCount;
+    }
+
+    function radioChanged(event, n) {
+        var radio = document.getElementById(event.target.id);
+        var radioIndex = [...radio.parentElement.children].indexOf(radio);
+        var slides = radio.parentElement.parentElement.parentElement.getElementsByClassName('slides')[0];
+        slides.scrollLeft = radioIndex / slides.childElementCount * slides.scrollWidth;
+    }
+
+    function scrolled(event) {
+        var id = event.target.parentElement.id;
+        var slides = document.getElementById(id).getElementsByClassName('slides')[0];
+        var scrollRatio = slides.scrollLeft / slides.scrollWidth;
+
+        var radioId = 'radio_' + id + '_slide';
+        var size = slides.childElementCount;
+
+        for (let i = 1; i <= size; i++) {
+            if (scrollRatio + 0.5 / size < i / size) {
+                document.getElementById(radioId + i).checked = true;
+
+                if (i == 1) {
+                    document.getElementById(id).getElementsByClassName('previous')[0].style.visibility = "hidden";
+                } else {
+                    document.getElementById(id).getElementsByClassName('previous')[0].style.visibility = "visible";
+                }
+
+                if (i == size) {
+                    document.getElementById(id).getElementsByClassName('next')[0].style.visibility = "hidden";
+                } else {
+                    document.getElementById(id).getElementsByClassName('next')[0].style.visibility = "visible";
+                }
+
+                break;
+            }
+        }
+    }
+
+    document.querySelectorAll('.slider').forEach(
+        slider => {
+            slider.getElementsByClassName('previous')[0].style.visibility = "hidden";
+
+            if (slider.childElementCount < 1) {
+                slider.getElementsByClassName('next')[0].style.visibility = "hidden";
+            }
+
+            slider.querySelectorAll('.slider-button.previous img')[0].addEventListener(
+                'click', event => arrowClicked(event, -1)
+            );
+
+            slider.querySelectorAll('.slider-button.next img')[0].addEventListener(
+                'click', event => arrowClicked(event, 1)
+            );
+
+            slider.addEventListener(
+                'change', event => { radioChanged(event); }
+            );
+
+            slider.getElementsByClassName('slides')[0].addEventListener(
+                'scroll', event => scrolled(event)
+            );
+        }
+    );
+});
